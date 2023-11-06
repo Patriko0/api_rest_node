480c8d7d8322d83dbe7b14d914f4ef64ded785b5
Patrik Americo <patrikamerico5@gmail.com>
2023-11-06 19:07:05 -0300

    User migration e model

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
   },
 };
---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome teve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email ja existente no sistema',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email invalido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha prescisa ter entre 6 a 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
