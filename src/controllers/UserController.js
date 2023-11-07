1bf9e0df1d42f1cea22f7bd3317c99e43dd7dfe8
Patrik Americo <patrikamerico5@gmail.com>
2023-11-07 14:49:57 -0300

    User CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,62 @@ class UserController {
   async store(req, res) {
     try {
       const user = await User.create(req.body);
-      res.json(user);
+      return res.json(user);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) return res.status(400).json({ errors: ['Id não enviado'] });
+
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) return res.status(400).json({ errors: ['Usuário não existe'] });
+
+      const novoUser = await user.update(req.body);
+
+      return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) return res.status(400).json({ errors: ['Id não enviado'] });
+
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) return res.status(400).json({ errors: ['Usuário não existe'] });
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
 }
