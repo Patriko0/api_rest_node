7efba928d2fdb1fe9369a734b0ff88bf6105e37a
Patrik Americo <patrikamerico5@gmail.com>
2023-11-07 15:19:45 -0300

    Token adicionado

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 import './src/database';
 
@@ -23,6 +24,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
