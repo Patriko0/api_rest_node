5b81c09fbdac38fb5531800738c60fd85db1597e
Patrik Americo <patrikamerico5@gmail.com>
2023-11-07 19:35:14 -0300

    Alterando tabela de alunos no banco de dados

---
+++ src/database/migrations/20231107222641-mudar-email-aluno-unique.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      'alunos',
+      'email',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    );
+  },
+
+  async down() {},
+};
