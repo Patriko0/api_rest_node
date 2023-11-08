5db62d7a36fa0a58d9382cea604d703e0b2c0eac
Patrik Americo <patrikamerico5@gmail.com>
2023-11-08 16:54:18 -0300

    Tratando os arquivos no upload

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('arquivo'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
