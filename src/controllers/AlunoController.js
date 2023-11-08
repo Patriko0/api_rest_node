4088b58e049284faaca5bfafdfcd9c3e306fb317
Patrik Americo <patrikamerico5@gmail.com>
2023-11-08 19:12:42 -0300

    Associando alunos com fotos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,8 +1,16 @@
 import Aluno from '../models/Aluno';
+import Foto from '../models/Foto';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['id', 'filename'],
+      },
+    });
     return res.json(alunos);
   }
 
@@ -24,7 +32,14 @@ class AlunoController {
 
       if (!id) return res.status(400).json({ errors: ['Faltando ID'] });
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+        order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+        include: {
+          model: Foto,
+          attributes: ['id', 'filename'],
+        },
+      });
 
       if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
 
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -71,4 +71,8 @@ export default class Aluno extends Model {
     });
     return this;
   }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: 'aluno_id' });
+  }
 }
