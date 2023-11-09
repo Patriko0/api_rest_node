7de284f02e606c3645fc4ff4583f928bf93d7d83
Patrik Americo <patrikamerico5@gmail.com>
2023-11-09 16:06:59 -0300

    Preparando para distribuição

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`\nEscutando na porta ${port}`);
-  console.log(`http://localhost:${port}`);
-});
--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import { resolve } from 'path';
 
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import fotoRoutes from './src/routes/fotoRoutes';
+import express from 'express';
 
-import './src/database';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fotoRoutes from './routes/fotoRoutes';
+
+import './database';
 
 dotenv.config();
 
@@ -23,7 +24,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
   }
 
   routes() {
