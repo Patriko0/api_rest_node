a328c00de15a4a90013513de088137c4bba8571d
Patrik Americo <patrikamerico5@gmail.com>
2023-11-13 18:02:41 -0300

    Adicionado CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import { resolve } from 'path';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
@@ -13,6 +15,23 @@ import './database';
 
 dotenv.config();
 
+const whiteList = [
+  'http://192.168.0.187/',
+  'http://localhost:3000',
+//   'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      console.log(whiteList, origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -22,6 +41,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -20,7 +20,7 @@ class TokenController {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
